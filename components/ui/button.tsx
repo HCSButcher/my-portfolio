d77614c4aa0effe39463469db78488dbe49ac4aa
dev-Butcher <trevor254oduol@gmail.com>
2025-12-23 15:06:53 +0300

    edited ui button

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,24 +1,23 @@
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
-import { cva } from "class-variance-authority";
-
+import { cva, VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
+// Define button variants using cva
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-full text-base font-semibold ring-offset-white transition-colors ",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-full text-base font-semibold ring-offset-white transition-colors",
   {
     variants: {
       variant: {
-        default:
-          "bg-[#00ff99] text-primary  shadow-xs hover:bg-[#00e187]-hover",
+        default: "bg-[#00ff99] text-primary shadow-xs hover:bg-[#00e187]",
         primary: "bg-primary text-white",
         outline:
-          "border border-[#00ff99] bg-transparent text-[] hover:bg-[#00e187] hover:text-primary ",
+          "border border-[#00ff99] bg-transparent text-[#00ff99] hover:bg-[#00e187] hover:text-primary",
       },
       size: {
-        default: "h-[44px] px-6  ",
-        md: "h-[48px] px-6 ",
-        lg: "h-[56px] px-8 text-sm uppercase tracking-[2px] ",
+        default: "h-[44px] px-6",
+        md: "h-[48px] px-6",
+        lg: "h-[56px] px-8 text-sm uppercase tracking-[2px]",
       },
     },
     defaultVariants: {
@@ -28,7 +27,21 @@ const buttonVariants = cva(
   }
 );
 
-function Button({ className, variant, size, asChild = false, ...props }) {
+// Define TypeScript props for the button
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+}
+
+// Button component
+function Button({
+  className,
+  variant,
+  size,
+  asChild = false,
+  ...props
+}: ButtonProps) {
   const Comp = asChild ? Slot : "button";
 
   return (
